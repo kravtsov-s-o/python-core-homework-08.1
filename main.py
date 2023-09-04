630329c34fe4c4987650bdf506e0ca99be2dd524
s.kravtsov <s.kravtsov@hotmail.com>
2023-09-04 22:26:15 +0300

    init

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ from datetime import date, datetime
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+    weekends = ['Saturday', 'Sunday']
+    birthdays_per_week = dict()
+    current_date = date.today()
+
+    if not users:
+        return birthdays_per_week
+
+    for user in users:
+        name = user['name']
+        birthday = user['birthday']
+
+        if birthday.year < current_date.year:
+            birthday = birthday.replace(year=current_date.year + 1)
+
+        if birthday < current_date:
+            continue
+
+        birthday_weekday = birthday.strftime('%A')
+
+        if birthday_weekday in weekends:
+            birthday_weekday = 'Monday'
+
+        birthdays_per_week.setdefault(birthday_weekday, []).append(name)
+
+    return birthdays_per_week
 
 
 if __name__ == "__main__":
